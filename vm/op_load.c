b6134fb5aebb6143ec7856683a8c2900e0a58c70
Rostislav KULAHIN <rkulahin@e1r1p17.unit.ua>
2019-02-22 09:07:49 +0200

    all

---
+++ vm/op_load.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_load.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seshevch <seshevch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/19 13:11:41 by seshevch          #+#    #+#             */
+/*   Updated: 2019/02/21 17:39:37 by seshevch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm.h"
+
+static int		convert_ind(t_vm *vm, t_carriage *car, char *str)
+{
+	int			numb;
+
+	numb = vm_atoi_16(valid_str(vm,
+	(car->position - 2 + ((vm_atoi_16(str) % IDX_MOD) * 2)) % 8192, 8));
+	return (numb);
+}
+
+static void		push_position(int *arg, t_carriage *car)
+{
+	int			i;
+
+	i = -1;
+	car->position = (car->position + 4) % 8192;
+	while (++i < 2)
+	{
+		if (arg[i] == T_REG)
+			car->position = (car->position + 2) % 8192;
+		else if (arg[i] == T_IND)
+			car->position = (car->position + 4) % 8192;
+		else if (arg[i] == T_DIR)
+			car->position = (car->position + 8) % 8192;
+	}
+}
+
+void			op_load(t_vm *vm, t_carriage *car)
+{
+	int				*arg;
+	char			*str;
+	unsigned char	reg;
+
+	arg = check_arg(vm_atoi_16(valid_str(vm, car->position, 2)));
+	if (arg[0] == T_DIR && arg[1] == T_REG)
+	{
+		str = valid_str(vm, (car->position + 2) % 8192, 10);
+		reg = (unsigned char)vm_atoi_16(str + 8);
+		str[8] = '\0';
+		car->regist[reg] = (unsigned int)vm_atoi_16(str);
+		car->carry = car->regist[reg] == 0 ? 1 : 0;
+	}
+	else if (arg[0] == T_IND && arg[1] == T_REG)
+	{
+		str = valid_str(vm, (car->position + 2) % 8192, 6);
+		reg = (unsigned char)vm_atoi_16(str + 4);
+		str[4] = '\0';
+		car->regist[reg] = (unsigned int)convert_ind(vm, car, str);
+		car->carry = car->regist[reg] == 0 ? 1 : 0;
+	}
+	push_position(arg, car);
+}
---
+++ vm/op_long_load_index.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_long_load_index.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seshevch <seshevch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/21 17:49:54 by seshevch          #+#    #+#             */
+/*   Updated: 2019/02/21 17:53:46 by seshevch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm.h"
+
+static int		*cast_arg_norm(int type[3], char *str)
+{
+	int			arg[3];
+
+	arg[2] = (unsigned char)vm_atoi_16(str + ((type[0] + type[1]) * 2));
+	str[(type[0] + type[1]) * 2] = '\0';
+	if (type[1] == 1)
+		arg[1] = (unsigned char)vm_atoi_16(str + (type[0] * 2));
+	else
+		arg[1] = (short)vm_atoi_16(str + (type[0] * 2));
+	str[type[0] * 2] = '\0';
+	if (type[0] == 1)
+		arg[0] = (unsigned char)vm_atoi_16(str);
+	else
+		arg[0] = (short)vm_atoi_16(str);
+	return (arg);
+}
+
+static int		*cast_arg_ind(t_vm *vm, int type[3], char *str, int position)
+{
+	int			arg[3];
+
+	arg[2] = (unsigned char)vm_atoi_16(str + ((2 + type[1]) * 2));
+	str[(2 + type[1]) * 2] = '\0';
+	if (type[1] == 1)
+		arg[1] = (unsigned char)vm_atoi_16(str + 4);
+	else
+		arg[1] = (short)vm_atoi_16(str + 4);
+	str[4] = '\0';
+	arg[0] = (unsigned int)valid_str(vm, ((vm_atoi_16(str) % IDX_MOD) * 2 +
+											position - 2) % 8192, 8);
+	return (arg);
+}
+
+static void		push_position(int *tp, t_carriage *car)
+{
+	int			i;
+
+	i = -1;
+	car->position = (car->position + 4) % 8192;
+	while (++i < 3)
+	{
+		if (tp[i] == T_REG)
+			car->position = (car->position + 2) % 8192;
+		else if (tp[i] == T_IND || tp[i] == T_DIR)
+			car->position = (car->position + 4) % 8192;
+	}
+}
+
+void			op_load_index(t_vm *vm, t_carriage *cr)
+{
+	int				*tp;
+	int				*arg;
+	char			*str;
+
+	tp = check_arg(vm_atoi_16(valid_str(vm, cr->position, 2)));
+	if ((tp[0] == 1 || tp[0] == 2) && (tp[1] == 1 || tp[1] == 2) && tp[2] == 1)
+	{
+		str = valid_str(vm, (cr->position + 2) % 8192,
+				(tp[0] + tp[1] + tp[2]) * 2);
+		arg = cast_arg_norm(tp, str);
+		cr->regist[arg[2]] = (unsigned int)vm_atoi_16(valid_str(vm,
+		((arg[0] + arg[1]) * 2 + cr->position - 2) % 8192, 8));
+		cr->carry = cr->regist[arg[2]] == 0 ? 1 : 0;
+	}
+	else if (tp[0] == 4 && (tp[1] == 1 || tp[1] == 2) && tp[2] == 1)
+	{
+		str = valid_str(vm, (cr->position + 2) % 8192,
+				(2 + tp[1] + tp[2]) * 2);
+		arg = cast_arg_ind(vm, tp, str, cr->position);
+		cr->regist[arg[2]] = (unsigned int)vm_atoi_16(valid_str(vm,
+		((arg[0] + arg[1]) * 2 + cr->position - 2) % 8192, 8));
+		cr->carry = cr->regist[arg[2]] == 0 ? 1 : 0;
+	}
+	push_position(tp, cr);
+}
